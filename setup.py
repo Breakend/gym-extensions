4c3bb9601093927ea01a2a764c88ed30fbd2c1ac
Peter Henderson <peter.henderson@mail.mcgill.ca>
2017-06-13 12:52:50 -0400

    Update setup and add docs folder for webpage

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 setup(name='gym_extensions',
       version='0.0.1',
-      install_requires=['gym', 'pyrr']  # And any other dependencies foo needs
+      install_requires=['gym', 'pyrr'],  # And any other dependencies foo needs
+      description='Extensions to OpenAI Gym for multitask learning, inverse reinforcement learning, and other peripheral tasks.',
+      author='Peter Henderson',
+      url='https://github.com/Breakend/gym-extensions',
 )
