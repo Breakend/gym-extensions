33f0196dc107cf9cc3b803e141d86d0e323f6750
Peter Henderson <peter.henderson@mail.mcgill.ca>
2017-07-15 14:44:42 -0400

    Add some missing dpendencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='gym_extensions',
       version='0.0.1',
       packages=['gym_extensions'],
-      install_requires=['numpy>=1.10.4', 'mujoco_py', 'gym[all]>=0.9.2', 'pyrr', 'six', 'cached_property'],
+      install_requires=['numpy>=1.10.4', 'mujoco_py', 'gym[all]>=0.9.2', 'pyrr', 'six', 'cached_property', 'networkx', 'opencv-python'],
       description='Extensions to OpenAI Gym for multitask learning, inverse reinforcement learning, and other peripheral tasks.',
       author='Peter Henderson',
       url='https://github.com/Breakend/gym-extensions',
