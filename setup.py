1ea59c0146b3d72c49a3c7e5cd3b71b5808ed722
Peter Henderson <peter.henderson@mail.mcgill.ca>
2017-07-15 14:22:35 -0400

    remove pygame for now

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='gym_extensions',
       version='0.0.1',
       packages=['gym_extensions'],
-      install_requires=['numpy>=1.10.4', 'mujoco_py', 'gym[all]>=0.9.2', 'pyrr', 'six', 'pgyame', 'cached_property'],
+      install_requires=['numpy>=1.10.4', 'mujoco_py', 'gym[all]>=0.9.2', 'pyrr', 'six', 'cached_property'],
       description='Extensions to OpenAI Gym for multitask learning, inverse reinforcement learning, and other peripheral tasks.',
       author='Peter Henderson',
       url='https://github.com/Breakend/gym-extensions',
